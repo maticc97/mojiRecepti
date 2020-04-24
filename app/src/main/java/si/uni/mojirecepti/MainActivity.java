569bd3b7562902b5a4ae99192823137de04cead1
J-Bez <jb5442@student.uni-lj.si>
2020-04-24 18:23:01 +0200

    Added specific recipe screen.

--- app/src/main/java/si/uni/mojirecepti/MainActivity.java
+++ app/src/main/java/si/uni/mojirecepti/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -41,7 +42,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void addData() {
-        Cursor cursor = myDb.viewData();
+        Cursor cursor = myDb.recipeTitles();
 
         if (cursor.getCount() == 0) {
             Toast.makeText(this, "Nimate shranjenih receptov", Toast.LENGTH_SHORT).show();
@@ -56,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private class MyListAdapter extends ArrayAdapter<String> {
+        Cursor cursor = myDb.recipeTitles();
         private int layout;
         private List<String> mObjects;
         private MyListAdapter(Context context, int resource, List<String> objects) {
@@ -72,11 +74,22 @@ public class MainActivity extends AppCompatActivity {
                 convertView = inflater.inflate(layout, parent, false);
                 ViewHolder viewHolder = new ViewHolder();
                 viewHolder.title = (TextView) convertView.findViewById(R.id.list_item_text);
-                viewHolder.button = (Button) convertView.findViewById(R.id.list_item_btn);
+                viewHolder.moreButton = (Button) convertView.findViewById(R.id.show_more_btn);
+                viewHolder.deleteButton = (Button) convertView.findViewById(R.id.delete_btn);
                 convertView.setTag(viewHolder);
             }
             mainViewholder = (ViewHolder) convertView.getTag();
-            mainViewholder.button.setOnClickListener(new View.OnClickListener() {
+            mainViewholder.moreButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    cursor.moveToPosition(position);
+                    final String id = cursor.getString(0);
+                    Intent intent = new Intent(MainActivity.this, RecipeActivity.class);
+                    intent.putExtra("ID", id);
+                    startActivity(intent);
+                }
+            });
+            mainViewholder.deleteButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     data.remove(position);
@@ -91,7 +104,8 @@ public class MainActivity extends AppCompatActivity {
 
     public static class ViewHolder {
         TextView title;
-        Button button;
+        Button moreButton;
+        Button deleteButton;
     }
 
     public void onButtonShowPopupWindowClick(View view) {
---
+++ app/src/main/java/si/uni/mojirecepti/RecipeActivity.java
@@ -0,0 +1,67 @@
+package si.uni.mojirecepti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+
+public class RecipeActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+    String title;
+    String id;
+    String category;
+    String ingredients;
+    String process;
+
+    private ArrayList<String> data = new ArrayList<String>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+        id = getIntent().getStringExtra("ID");
+        myDb = new DatabaseHelper(this);
+
+        ListView lvItems = (ListView) findViewById(R.id.ingredientsList);
+
+        getData();
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+        lvItems.setAdapter(itemsAdapter);
+
+        TextView recipeTitle = (TextView)findViewById(R.id.recipeTitle);
+        TextView recipeCategory = (TextView)findViewById(R.id.recipeCategory);
+        TextView recipeProcess = (TextView)findViewById(R.id.recipeProcess);
+        recipeTitle.setText(title);
+        recipeCategory.setText(category);
+        recipeProcess.setText(process);
+    }
+
+    private void getData() {
+        Cursor cursor = myDb.getRecipe(id);
+        cursor.moveToFirst();
+        title = cursor.getString(1);
+        category = cursor.getString(2);
+        ingredients = cursor.getString(3);
+        String [] test = ingredients.split(",");
+        for (String ingredient : test) {
+            String fixed = ingredient.replaceAll("[\\[\\]]", "");
+            data.add(fixed);
+        }
+        process = cursor.getString(4);
+    }
+
+    public void onButtonBackClick(View view) {
+        Intent intent = new Intent(RecipeActivity.this, MainActivity.class);
+        startActivity(intent);
+    }
+}
