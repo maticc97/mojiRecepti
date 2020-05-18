9758b8abda1db0edf98da5f6b4f0a63d4ef9a8c5
J-Bez <jb5442@student.uni-lj.si>
2020-05-18 11:53:21 +0200

    Delete ingredient funcionality.

--- app/src/main/java/si/uni/mojirecepti/addRecipeActivity.java
+++ app/src/main/java/si/uni/mojirecepti/addRecipeActivity.java
@@ -1,11 +1,17 @@
 package si.uni.mojirecepti;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -14,9 +20,11 @@ import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class addRecipeActivity extends AppCompatActivity {
 
@@ -36,6 +44,7 @@ public class addRecipeActivity extends AppCompatActivity {
     Layout a;
     ArrayList<String> arrayList;
     ArrayAdapter<String> adapter;
+    ArrayAdapter<String> adapter1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,7 +72,10 @@ public class addRecipeActivity extends AppCompatActivity {
 
 
         arrayList = new ArrayList<String>();
-        adapter = new ArrayAdapter<String>(addRecipeActivity.this, R.layout.list_item_layout, R.id.lst_txt, arrayList);
+        //adapter1 = new ArrayAdapter<String>(addRecipeActivity.this, R.layout.list_item_layout, R.id.lst_txt, arrayList);
+        //lv.setAdapter(adapter1);
+
+        adapter = new MyAdapter(this, R.layout.list_item_layout, arrayList);
         lv.setAdapter(adapter);
         dodajSestavino();
         AddData();
@@ -72,8 +84,46 @@ public class addRecipeActivity extends AppCompatActivity {
 
     }
 
+    private class MyAdapter extends ArrayAdapter<String> {
+        private int layout;
+        private List<String> mObjects;
+        private MyAdapter(Context context, int resource, List<String> objects) {
+            super(context, resource, objects);
+            mObjects = objects;
+            layout = resource;
+        }
 
+        @Override
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            addViewHolder mainViewholder = null;
+            if(convertView == null) {
+                LayoutInflater inflater = LayoutInflater.from(getContext());
+                convertView = inflater.inflate(layout, parent, false);
+                addViewHolder viewHolder = new addViewHolder();
+                viewHolder.title = (TextView) convertView.findViewById(R.id.lst_txt);
+                viewHolder.deleteButton = (ImageButton) convertView.findViewById(R.id.odstranisest);
+                convertView.setTag(viewHolder);
+            }
+            mainViewholder = (addViewHolder) convertView.getTag();
+            mainViewholder.deleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    System.out.println(position);
+                    System.out.println(arrayList);
+                    arrayList.remove(position);
+                    adapter.notifyDataSetChanged();
+                }
+            });
+            mainViewholder.title.setText(getItem(position));
 
+            return convertView;
+        }
+    }
+
+    public static class addViewHolder {
+        TextView title;
+        ImageButton deleteButton;
+    }
 
     @Override
     public void finish() {
